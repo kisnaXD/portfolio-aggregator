8778652486fcc15ed60c8bd220e093cf5f5363b3
kisnaXD <krishnagera93@gmail.com>
2025-06-26 02:20:24 +0530

    Scrollability achieved, sign up feature left

--- PortfolioAggregator/src/components/SignUpLeft.jsx
+++ PortfolioAggregator/src/components/SignUpLeft.jsx
@@ -1,58 +1,51 @@
-import React from 'react'
-import '../styling/SignUpLeft.css'
-import brokerLogo from '../assets/broker_logo_dummy.png'
-import { IoIosHome } from "react-icons/io";
-import { CiLock } from "react-icons/ci";
-import { BsThreeDotsVertical } from "react-icons/bs";
-import { TbSquareNumber1 } from "react-icons/tb";
+import React from 'react';
+import '../styling/SignUpLeft.css';
+import brokerLogo from '../assets/broker_logo_dummy.png';
+import { IoIosHome } from 'react-icons/io';
+import { CiLock } from 'react-icons/ci';
+import { BsThreeDotsVertical } from 'react-icons/bs';
+import { TbSquareNumber1 } from 'react-icons/tb';
 
 const SignUpLeft = () => {
+    const brokers = [
+        { id: 1, name: 'Zerodha' },
+        { id: 2, name: 'Upstox' },
+        { id: 3, name: 'Angel One' },
+        { id: 4, name: 'Groww' },
+        { id: 5, name: 'ICICI Direct' },
+        { id: 6, name: 'HDFC Securities' },
+        { id: 7, name: 'Kotak Securities' },
+        { id: 8, name: 'Sharekhan' },
+        { id: 9, name: 'Motilal Oswal' },
+        { id: 10, name: '5paisa' },
+    ];
+
     return (
         <div className="sign-up-left-container">
-            <h1 className="sign-up-left-heading">Portfolio <br></br>Aggregator</h1>
+            <h1 className="sign-up-left-heading">
+                Portfolio <br /> Aggregator
+            </h1>
             <p className="sign-up-left-byline">Brokers we Support</p>
             <div className="sign-up-left-phone">
                 <div className="sign-up-left-header">
-                    <IoIosHome className="phone-icon"/>
+                    <IoIosHome className="phone-icon" />
                     <div className="search-bar">
-                        <CiLock className="phone-icon" id="lock-icon"/> 
+                        <CiLock className="phone-icon" id="lock-icon" />
                     </div>
-                    <TbSquareNumber1 className="phone-icon"/>
-                    <BsThreeDotsVertical className="phone-icon"/>
+                    <TbSquareNumber1 className="phone-icon" />
+                    <BsThreeDotsVertical className="phone-icon" />
                 </div>
                 <div className="sign-up-left-section">
-                    <div className="broker-section">
-                        <img src={brokerLogo} className="broker-logo" />
-                        <p className="broker-name">Broker 1</p>
-                    </div>
-                    <div className="broker-section">
-                        <img src={brokerLogo} className="broker-logo" />
-                        <p className="broker-name">Broker 2</p>
-                    </div>
-                    <div className="broker-section">
-                        <img src={brokerLogo} className="broker-logo" />
-                        <p className="broker-name">Broker 3</p>
-                    </div>
-                    <div className="broker-section">
-                        <img src={brokerLogo} className="broker-logo" />
-                        <p className="broker-name">Broker 4</p>
-                    </div>
-                    <div className="broker-section">
-                        <img src={brokerLogo} className="broker-logo" />
-                        <p className="broker-name">Broker 5</p>
-                    </div>
-                    <div className="broker-section">
-                        <img src={brokerLogo} className="broker-logo" />
-                        <p className="broker-name">Broker 6</p>
-                    </div>
-                    <div className="broker-section">
-                        <img src={brokerLogo} className="broker-logo" />
-                        <p className="broker-name">Broker 7</p>
-                    </div>
+                    {brokers.map(broker => (
+                        <div key={broker.id} className="broker-section">
+                            <img src={brokerLogo} alt={broker.name} className="broker-logo" />
+                            <p className="broker-name">{broker.name}</p>
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default SignUpLeft
+export default SignUpLeft;
